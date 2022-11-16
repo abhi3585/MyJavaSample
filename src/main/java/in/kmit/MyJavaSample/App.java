dfdd11e2f3e63a3023fdf742d338e9d6579ce699
Abhinavyadav <abhinavyadavabhinav78474@gmail.com>
2022-11-16 11:50:50 +0530

    java project

---
+++ src/main/java/in/kmit/MyJavaSample/App.java
@@ -0,0 +1,13 @@
+package in.kmit.MyJavaSample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
